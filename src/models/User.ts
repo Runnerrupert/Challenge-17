7424a7433e986dba8726537c229d9e02cd13ef27
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-10-06 10:40:11 -0600

    Add User Schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,43 @@
-import { Schema, model, document, ObjectID } from 'mongoose';
+import { Schema, model, type Document } from 'mongoose';
+
+interface IUser extends Document {
+    username: string,
+    email: string,
+    thoughts: Schema.Types.ObjectId[],
+    friends: Schema.Types.ObjectId[]
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        timestamps: true
+    },
+);
+
+const User = model<IUser>('User', userSchema);
 
 export default User;
